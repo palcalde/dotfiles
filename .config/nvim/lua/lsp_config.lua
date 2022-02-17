da387078ea6744a13a53efb2cd31394746a8a761
Pablo Alcalde <pablo.alcalde@cabify.com>
2022-02-17 08:11:23 +0100

    Remove vim-go highlighting

--- .config/nvim/lua/lsp_config.lua
+++ .config/nvim/lua/lsp_config.lua
@@ -70,11 +70,7 @@ require'nvim-treesitter.configs'.setup {
 
   highlight = {
     enable = true,
-
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
+    -- Setting this to true will run `syntax` highlighting with tree-sitter at the same time
     additional_vim_regex_highlighting = false,
   },
 }
