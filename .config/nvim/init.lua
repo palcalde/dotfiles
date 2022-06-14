8fa4e717d16f47ce84d72ae5eb8f1d31bd336c64
Pablo Alcalde <pablo.alcalde@cabify.com>
2022-06-14 12:08:54 +0200

    Move from init.vim to init.lua

---
+++ .config/nvim/init.lua
@@ -0,0 +1,11 @@
+local opt = vim.opt
+
+opt.wildignore:prepend { "~/.vim", "runtimepath" }
+opt.wildignore:append { "~/.vim/after", "runtimepath" }
+
+vim.cmd("source ~/.vimrc")
+
+-- Load configs
+require("lsp_config")
+require("cmp_config")
+require("treesitter_config")
