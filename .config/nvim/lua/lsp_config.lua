f0d19c86a08b5f85160e4d04faecc5831b952717
Pablo Alcalde <pablo.alcalde@cabify.com>
2022-06-14 10:43:41 +0200

    Add illuminate plugin to highlight word under cursor

--- .config/nvim/lua/lsp_config.lua
+++ .config/nvim/lua/lsp_config.lua
@@ -2,6 +2,7 @@
 -- Use an on_attach function to only map the following keys 
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
+  require 'illuminate'.on_attach(client)
   require "lsp_signature".on_attach(clinet)
 
   local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
