9babc6b5c4502b1b75c9771e0599189dff531067
Pablo Alcalde <pablo.alcalde@cabify.com>
2023-02-24 09:24:48 +0100

    fix treesitter open at startup

--- .config/nvim/lua/tree_config.lua
+++ .config/nvim/lua/tree_config.lua
@@ -56,3 +56,27 @@ require'nvim-tree'.setup {
 
 -- exit vim after closing last opened buff
 vim.cmd("autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif")
+
+-- configure nvim to open on startup
+-- https://github.com/nvim-tree/nvim-tree.lua/wiki/Open-At-Startup
+local function open_nvim_tree(data)
+  -- buffer is a [No Name]
+  local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
+
+  -- buffer is a directory
+  local directory = vim.fn.isdirectory(data.file) == 1
+
+  if not no_name and not directory then
+    return
+  end
+
+  -- change to the directory
+  if directory then
+    vim.cmd.cd(data.file)
+  end
+
+  -- open the tree
+  require("nvim-tree.api").tree.open()
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
