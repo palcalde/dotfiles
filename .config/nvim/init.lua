2298c662f03c952a66ddc4f1911bf0442c901e04
Pablo Alcalde <pablo.alcalde@cabify.com>
2025-04-04 22:43:26 +0200

    add config for vscode

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,12 +3,15 @@ local opt = vim.opt
 opt.wildignore:prepend { "~/.vim", "runtimepath" }
 opt.wildignore:append { "~/.vim/after", "runtimepath" }
 
-vim.cmd("source ~/.vimrc")
+-- Check if vim.g.vscode exists
+if vim.g.vscode == nil then
+	vim.cmd("source ~/.vimrc")
+	-- Load configs
+	require("lsp_config")
+	require("cmp_config")
+	require("treesitter_config")
+	require("tree_config")
+	require("telescope_config")
+	require("supermaven_config")
+end
 
--- Load configs
-require("lsp_config")
-require("cmp_config")
-require("treesitter_config")
-require("tree_config")
-require("telescope_config")
-require("supermaven_config")
